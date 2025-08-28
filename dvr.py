9c5f68f949d4e799e6a6ff65f0f33560c3c903e0
KiarashMafi <k.kiarashmafi@gmail.com>
2025-08-28 11:26:18 +0330

    Anomaly data points detetcted based on z-score

--- dvr.py
+++ dvr.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import psycopg2
+
 from preprocessor import main
+
+conn = psycopg2.connect(
+    dbname="oilrig",
+    user="postgres",
+    password="1234",
+    host="localhost",
+    port=5432
+)
+cursor = conn.cursor()
 
 df = pd.read_csv("output_data/rig_data_10000.csv")
 
@@ -26,6 +37,7 @@ selected_features = [
 z_scores = {}
 parameters = {}
 z_score_threshold = 3
+test = set()
 
 for i in selected_features:
     mean, std = df[i].mean(), df[i].std()
@@ -36,9 +48,14 @@ for _, row in df.iterrows():
     for key in selected_features:
         x = row[key]
         z_score = (x-parameters[key][0])/parameters[key][1]
-        print(f"Parameters:‌{parameters}")
-        print(f"{key} value: {row[key]}, z score:‌{z_score}")
-        # if abs(z_score) > 3:
-            # print(f"{key}: {row[key]} is anomaly data")
+        # print(f"Parameters:‌{parameters}")
+        # print(f"{key} value: {row[key]}, z score:‌{z_score}")
+        if abs(z_score) > z_score_threshold:
+            cursor.execute(
+                "UPDATE sensor_data SET zscore_anomaly = TRUE WHERE timestamp = %s", (row["Timestamp"],)
+            )
 
+conn.commit()
+cursor.close()
+conn.close()
 # Token: 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
